9d6dbc81b5efe0a77747b8e92178c8034de37651
sudarsan21 <110298211+sudarsan21@users.noreply.github.com>
2022-08-10 16:22:22 +0530

    Add files via upload

--- EX15.py
+++ EX15.py
@@ -1,19 +1,18 @@
-with open('file.txt','w')as f:
-    n=int(input('Enter the number of lines of data:'))
+with open('file1.txt','w')as f:
+    n=int(input('enter the number of lines of data:'))
     for i in range(n):
-        data=input('Enter the next line of data:')
+        data=input('enter the next line of data:')
         f.write(data+'\n')
-    with open('file.txt','r')as f:
-      with open('files.txt','w')as s:
-        for i in f:
+with open('file1.txt','r')as f:
+   with open ('file2.txt','w')as s:
+       for i in f:
             s.write(i)
-            with open('files.txt','r')as s:
-                print('The content of  file 2 is:\n',s.read())
-            with open('file1'
-                      '.txt','r')as f:
-                d=f.read()
-            l=d.split()
-            count=0
-            for i in l:
-                cout=count+l
-            print("Number  of words  copied from file1 to file2 is:",count)
+   with open('file2.txt','r')as s:
+        print('the content of file2 is:\n',s.read())
+   with open('file1.txt','r')as f:
+       d=f.read()
+       l=d.split()
+       count=0
+for i in l:
+          count=count+1
+print('number of words copied from file1 to file2 is:',count)
